46f7e1a9bca922363646fb65d4392f7f1a40facf
Kuuku Ampah <ampah.kuuku@gmail.com>
2024-05-26 10:31:30 +0000

    made my name bold

--- AssignmentKuuku/App.js
+++ AssignmentKuuku/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Kuuku</Text>
+      <Text style={styles.text}>My name is <Text style={{fontWeight: 'bold'}}>Kuuku</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
